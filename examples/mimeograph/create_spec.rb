4c0284d5cd11eab696ba3600617f0f926f3c074e
qxjit (David Vollbracht) <david.vollbracht@gmail.com>
2009-10-23 11:34:20 -0400

    David: preserve symlinks

--- examples/mimeograph/create_spec.rb
+++ examples/mimeograph/create_spec.rb
@@ -4,27 +4,32 @@ describe "mimegraph create" do
   include SpecRig::Helpers
 
   it "should copy all files from source to destination" do
-    workspace.source.join("file1").create("content1")
-    workspace.source.join("subdir", "file2").create("content2")
+    source.join("file1").create("content1")
+    source.join("subdir", "file2").create("content2")
 
-    mimeograph(:create, workspace.source, workspace.destination).should be_successful
+    mimeograph(:create, source, destination).should be_successful
 
-    workspace.destination.join("file1").read.should == "content1"
-    workspace.destination.join("subdir", "file2").read.should == "content2"
+    destination.join("file1").read.should == "content1"
+    destination.join("subdir", "file2").read.should == "content2"
   end
 
   it "should perserve permissions" do
-    workspace.source.join("file").create.chmod(0666)
-
-    mimeograph(:create, workspace.source, workspace.destination).should be_successful
-
-    (workspace.destination.join("file").stat.mode & 0777).should == 0666
+    source.join("file").create.chmod(0666)
+    mimeograph(:create, source, destination).should be_successful
+    (destination.join("file").stat.mode & 0777).should == 0666
   end
 
   it "should preserve timestamps" do
-    workspace.source.join("file").create.utime Time.now, Time.parse("12/2/2008")
-    mimeograph(:create, workspace.source, workspace.destination).should be_successful
-    workspace.destination.join("file").stat.mtime.should == workspace.source.join("file").stat.mtime
+    source.join("file").create.utime Time.now, Time.parse("12/2/2008")
+    mimeograph(:create, source, destination).should be_successful
+    destination.join("file").stat.mtime.should == source.join("file").stat.mtime
+  end
+
+  it "should preserve symlinks" do
+    source.join("symlink").make_symlink("target")
+    mimeograph(:create, source, destination).should be_successful
+    destination.join("symlink").should be_symlink
+    destination.join("symlink").readlink.to_s.should == "target"
   end
 
   def mimeograph(*args)
--- examples_lib/spec_rig/helpers.rb
+++ examples_lib/spec_rig/helpers.rb
@@ -3,5 +3,13 @@ module SpecRig
     def workspace
       @workspace ||= Workspace.new
     end
+
+    def source
+      workspace.source
+    end
+
+    def destination
+      workspace.destination
+    end
   end
 end
